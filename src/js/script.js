f83af341961a6acbf012b0a2c9622f0e7e994856
Michal Borek <borekmichal30@gmail.com>
2020-03-22 23:20:28 +0100

    Add acordeon

---
+++ src/js/script.js
@@ -0,0 +1,139 @@
+/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+
+{
+  'use strict';
+
+  const select = {
+    templateOf: {
+      menuProduct: '#template-menu-product',
+    },
+    containerOf: {
+      menu: '#product-list',
+      cart: '#cart',
+    },
+    all: {
+      menuProducts: '#product-list > .product',
+      menuProductsActive: '#product-list > .product.active',
+      formInputs: 'input, select',
+    },
+    menuProduct: {
+      clickable: '.product__header',
+      form: '.product__order',
+      priceElem: '.product__total-price .price',
+      imageWrapper: '.product__images',
+      amountWidget: '.widget-amount',
+      cartButton: '[href="#add-to-cart"]',
+    },
+    widgets: {
+      amount: {
+        input: 'input[name="amount"]',
+        linkDecrease: 'a[href="#less"]',
+        linkIncrease: 'a[href="#more"]',
+      },
+    },
+  };
+
+  const classNames = {
+    menuProduct: {
+      wrapperActive: 'active',
+      imageVisible: 'active',
+    },
+  };
+
+  const settings = {
+    amountWidget: {
+      defaultValue: 1,
+      defaultMin: 1,
+      defaultMax: 9,
+    }
+  };
+
+  const templates = {
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  };
+
+
+  class Product {
+	  constructor(id,data){
+		  const thisProduct = this;
+		  thisProduct.id = id;
+		  thisProduct.data = data;
+		  thisProduct.renderInMenu();
+		  thisProduct.initAccordion();
+		  console.log('new Product:', thisProduct);
+	  }
+	  
+	  renderInMenu(){
+		  const thisProduct = this;
+		  /*generate HTML based on template */
+		  const generatedHTML = templates.menuProduct(thisProduct.data);
+		  console.log('generatedHTML:', generatedHTML);
+		  /* create element using utils.createElementFromHTML */
+		  thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+	      /*find menu container*/
+		  const menuContainer = document.querySelector(select.containerOf.menu);
+		  /* add element to menu*/
+		  menuContainer.appendChild(thisProduct.element);
+	  }
+	  
+	  initAccordion(){
+	    const thisProduct = this;
+		/* find the clickable trigger (the element that should react to clicking) */
+         let clickedElement = thisProduct.element.querySelector(select.menuProduct.clickable);	
+    /* START: click event listener to trigger */
+        clickedElement.addEventListener('click', function(event){
+			console.log('clicked:', clickedElement);
+			/* prevent default action for event */
+			event.preventDefault();
+			/* toggle active class on element of thisProduct */
+			thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+			/* find all active products */
+			const activeProducts = document.querySelectorAll(select.all.menuProductActive);
+			console.log('found active products:', activeProducts);
+			/* START LOOP: for each active product */
+			for(let activeProduct in activeProducts){
+			  /* START: if the active product isn't the element of thisProduct */
+			  if(activeProduct !== clickedElement){
+				/* remove class active for the active product */
+                activeProduct.classList.remove(classNames.menuProduct.wrapperActive);  				
+			  }	  
+			}
+		});
+      }
+
+	  
+  }
+  
+  
+  const app = {
+	initMenu: function(){
+	  const thisApp = this;
+      
+	  console.log('thisApp.data:', thisApp.data);
+	  
+	  for(let productData in thisApp.data.products){
+		  new Product(productData, thisApp.data.products[productData]);
+	  }
+      
+	},
+    init: function(){
+      const thisApp = this;
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
+      
+	  thisApp.initData();
+	  thisApp.initMenu();
+	},
+    initData: function(){
+	  const thisApp = this;
+	  
+	  thisApp.data = dataSource;
+    },
+  };
+  
+	
+  app.init();
+}
